7941bb4364351ebe29b5065fa85cc5b031250214
kosta <konstantinos.papanikolaou@it-tem.de>
2025-10-17 12:23:48 +0200

    Ergänzungen mit sportvorhersage oder empfehlung + erweiterung csv Datei

--- fitness/fitness-prediction.py
+++ fitness/fitness-prediction.py
@@ -7,6 +7,7 @@ def benutzerEingabe():
     alter = int(input("Alter: "))
     gewicht = float(input("Gewicht (in kg): "))
     groesse = float(input("Größe (in cm): "))
+    geschlecht = input("Geschlecht (m oder w): ")
     fitnesslevel = input("Fitnesslevel (niedrig / mittel / hoch): ").lower()
     #interesse = input("Interesse (z.B. Teamsport, Ausdauer, Entspannung): ").lower()
 
@@ -15,10 +16,30 @@ def benutzerEingabe():
         "Alter": alter,
         "Gewicht": gewicht,
         "Groesse": groesse,
+        "Geschlecht": geschlecht,
         "Fitnesslevel": fitnesslevel,
         #"Interesse": interesse
     }
 
+
+def sport_vorhersage(daten):
+    alter = daten["Alter"]
+    gewicht = daten["Gewicht"]
+    geschlecht = daten["Geschlecht"]
+    fitness = daten["Fitnesslevel"]
+
+    #Erweiterung mit Geschlecht?
+    if fitness == "niedrig":
+        return "Yoga"
+    elif fitness == "mittel":
+        if gewicht > 90:
+            return "Radfahren oder Schwimmen"
+        else:
+            return "Fitnessstudio oder Joggen"
+    elif fitness == "hoch":
+        return "Fußball oder Basketball"
+    else:
+        return "Allgemeine Bewegung wie Walking"
 
 def datenSpeichern(daten, dateiname="fitness/nutzer_daten.csv"):
     datei_existiert = os.path.isfile(dateiname)
@@ -33,4 +54,7 @@ def datenSpeichern(daten, dateiname="fitness/nutzer_daten.csv"):
     print(f"\nDaten gespeichert in {dateiname}")
 
 nutzer_daten = benutzerEingabe()
+empfehlung = sport_vorhersage(nutzer_daten)
+nutzer_daten["Empfehlung"] = empfehlung
+print(f"\n Basierend auf deinen Angaben empfehlen wir: {empfehlung}")
 datenSpeichern(nutzer_daten)
